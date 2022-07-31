5ebf2684480e6e27e2ad8b004e9d496d62e5f27e
Cristopher Namchee <cristophernamchee12@gmail.com>
2022-07-31 13:48:03 +0700

    feat: add subscription in server side code

--- server/router.ts
+++ server/router.ts
@@ -4,17 +4,38 @@ import fetch from 'isomorphic-unfetch';
 
 import { ENDPOINT } from './constant';
 
+async function fetchQuote(): Promise<string> {
+  const response = await fetch(ENDPOINT);
+  const json = await response.json();
+
+  return json.content;
+}
+
 export const router = trpc.router()
   .query('getQuote', {
     async resolve(_) {
-      const response = await fetch(ENDPOINT);
-      const json = await response.json();
+      const quote = await fetchQuote();
 
       return {
-        data: json.content,
+        data: quote,
       };
     }
-});
+  })
+  .subscription('randomQuote', {
+    resolve() {
+      return new trpc.Subscription<{ data: string }>((emit) => {
+        const timer = setInterval(async () => {
+          const quote = await fetchQuote();
+
+          emit.data({ data: quote });
+        }, 10_000);
+
+        return () => {
+          clearInterval(timer);
+        }
+      });
+    }
+  });
 
 // export signature
 export type AppRouter = typeof router;
