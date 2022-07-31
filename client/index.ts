0a6ffd42d01179c790031792fed36d8aef36f11c
Cristopher Namchee <cristophernamchee12@gmail.com>
2022-08-01 06:50:25 +0700

    feat: add websocket capability

--- client/index.ts
+++ client/index.ts
@@ -1,14 +1,56 @@
 import type { AppRouter } from './../server/router';
+
 import { createTRPCClient } from '@trpc/client';
+import { httpLink } from '@trpc/client/links/httpLink';
+import { splitLink } from '@trpc/client/links/splitLink';
+import { createWSClient, wsLink } from '@trpc/client/links/wsLink';
+
+import ws from 'ws';
 
 import fetch from 'isomorphic-unfetch';
 
+const wsClient = createWSClient({
+  url: 'ws://localhost:3000/trpc',
+  WebSocket: ws as any,
+});
 const client = createTRPCClient<AppRouter>({
-  url: 'http://localhost:3000/trpc',
   fetch: fetch,
+  links: [
+    splitLink({
+      condition(op) {
+        return op.type === 'subscription';
+      },
+      true: wsLink({
+        client: wsClient,
+      }),
+      false: httpLink({
+        url: `http://localhost:3000/trpc`,
+      }),
+    }),
+  ],
 });
 
 (async () => {
   const quote = await client.query('getQuote');
   console.log(quote);
+
+  let count = 0;
+
+  const unsub = client.subscription('randomQuote', null, {
+    onNext(data) {
+      console.log(data);
+
+      count++;
+      if (count > 3) {
+        unsub();
+      }
+    },
+    onError(err) {
+      console.error('error', err);
+    },
+    onDone() {
+      console.log('done called - closing websocket');
+      wsClient.close();
+    },
+  });
 })();
