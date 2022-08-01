4381224a1eea250cb2cd0181fa958aab24d17de3
Cristopher Namchee <cristophernamchee12@gmail.com>
2022-08-01 21:17:28 +0700

    feat: create websocket server

--- server/index.ts
+++ server/index.ts
@@ -3,14 +3,18 @@ import fastify from 'fastify';
 import { createContext } from './context';
 import { router } from './router';
 import { PARAM_LENGTH } from './constant';
+
+import ws from '@fastify/websocket';
 import { fastifyTRPCPlugin } from '@trpc/server/adapters/fastify/dist/trpc-server-adapters-fastify.cjs';
 
 const server = fastify({
   maxParamLength: PARAM_LENGTH,
 });
 
+server.register(ws);
 server.register(fastifyTRPCPlugin, {
   prefix: '/trpc',
+  useWSS: true,
   trpcOptions: { router, createContext },
 });
 
